46c6d56e6192415ded829b13313a1ac9370d97bc
Parth Lahoti <121300023+parth5404@users.noreply.github.com>
2025-12-26 21:06:49 +0530

    Time: 1747 ms (5.03%) | Memory: 13.3 MB (81.20%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int bestClosingTime(string cus) {
-        int max=0,score=0,time=-1;
-        for(int i=0;i<cus.length();i++){
-            score+=(cus[i]=='Y')?1:-1;
-            if(score>max){
-                max=score;
-                time=i;
+        int sum = 0;
+        int ans = INT_MAX;
+        for (char& ch : cus) {
+            if (ch == 'Y')
+                sum++;
+        }
+        int n_cnt = 0;
+        int ret_ans = 0;
+        int y_cnt = 0;
+        for (int i = 0; i < cus.length(); i++) {
+
+            if (i >= 1) {
+                if (cus[i - 1] == 'N')
+                    n_cnt++;
+                if (cus[i - 1] == 'Y')
+                    y_cnt++;
+            }
+
+            int val = n_cnt + sum - (y_cnt);        
+            cout<<val<<endl;
+            if (val < ans) {
+                ans = val;
+                ret_ans = i;
             }
         }
-        return time+1;
+        if(n_cnt<ans){
+            ret_ans=cus.length();
+        }
+
+        return ret_ans;
     }
 };
