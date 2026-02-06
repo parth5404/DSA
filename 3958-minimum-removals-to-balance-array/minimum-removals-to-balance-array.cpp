98d782ae4f1b5cb7f0f0cf9c5d2d89b0dec18e5c
Parth Lahoti <parthlahoti5404@gmail.com>
2026-02-06 10:29:41 +0530

    Time: 27 ms (87.50%) | Memory: 105.1 MB (14.24%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-       
+        sort(nums.begin(), nums.end());     
         int i = 0;
         int j = 0;
         int ans = nums.size();
         while (j < nums.size()) {
             if ((long long)k * nums[i] < nums[j]) {
                 i++;
-                if(i>j){
-                    j=i;
-                }
+                // if(i>j){
+                //     j=i;
+                // }
             } else {
                 ans = min(ans, (int)nums.size() - (j - i + 1));
                 j++;
