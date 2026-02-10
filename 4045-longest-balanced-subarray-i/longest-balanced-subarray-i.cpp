a1281bc34584e36de268ca47313c157532fbfaba
Parth Lahoti <parthlahoti5404@gmail.com>
2026-02-10 16:38:05 +0530

    Time: 1868 ms (73.59%) | Memory: 530.4 MB (41.51%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            unordered_map<int, int> even;
+            unordered_map<int, int> odd;
+            for (int j = i; j < nums.size(); j++) {
+                if (nums[j] % 2 == 0)
+                    even[nums[j]]++;
+                else
+                    odd[nums[j]]++;
+
+                if (even.size() == odd.size())
+                    ans = max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+};
