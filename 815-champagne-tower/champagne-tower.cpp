4c813fd6f5c4b83a5ba4712c734881c411a35050
Parth Lahoti <parthlahoti5404@gmail.com>
2026-02-14 18:23:54 +0530

    Time: 21 ms (5.33%) | Memory: 47.3 MB (9.44%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -3,18 +3,15 @@ public:
     double champagneTower(int poured, int query_row, int query_glass) {
         vector<vector<double>> dp(105, vector<double>(105, 0.0));
         dp[0][0] = poured;
-
         for (int i = 0; i <= query_row; i++) {
             for (int j = 0; j <= i; j++) {
                 if (dp[i][j] > 1.0) {
                     double extra = dp[i][j] - 1.0;
-                    dp[i][j] = 1.0;
-                    dp[i+1][j] += extra / 2.0;
-                    dp[i+1][j+1] += extra / 2.0;
+                    dp[i + 1][j] += extra / 2.0;
+                    dp[i + 1][j + 1] += extra / 2.0;
                 }
             }
         }
-
         return min(1.0, dp[query_row][query_glass]);
     }
 };
