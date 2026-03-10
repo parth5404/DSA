ad061b4af118477bd0b3f03e4e09b464463a55bd
Parth Lahoti <parthlahoti5404@gmail.com>
2026-03-10 07:23:48 +0530

    Time: 243 ms (60.16%) | Memory: 8.9 MB (71.54%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>> dp(
-            zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2)));
-        long long mod = 1e9 + 7;
-        for (int i = 0; i <= min(zero, limit); i++) {
-            dp[i][0][0] = 1;
+    int mod = 1e9 + 7;
+    int dp[202][202][2];
+    int solve(int zero, int one, int lt, int prev) {
+        if (zero == 0 && one == 0) {
+            return 1;
         }
-        for (int j = 0; j <= min(one, limit); j++) {
-            dp[0][j][1] = 1;
+        if (dp[zero][one][prev] != -1) {
+            return dp[zero][one][prev];
         }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1] -
-                                  dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
-                if (j > limit) {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0] -
-                                  dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+        int ans = 0;
+        for (int i = 1; i <= min(lt, (prev == 1) ? zero : one); i++) {
+            int val1 = zero;
+            int val2 = one;
+            if (prev == 1) {
+                val1 = val1 - i;
+            } else {
+                val2 = val2 - i;
             }
+            ans = (ans + solve(val1, val2, lt, (prev == 1) ? 0 : 1)) % mod;
         }
-        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+        return dp[zero][one][prev] = (ans) % mod;
+    }
+    int numberOfStableArrays(int zero, int one, int lt) {
+        memset(dp,-1,sizeof(dp));
+        return (solve(zero, one, lt, 0) + solve(zero, one, lt, 1)) % mod;
     }
 };
