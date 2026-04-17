0252585441a78209a790414f15de2e6cc5b9eb32
Parth Lahoti <parthlahoti5404@gmail.com>
2026-04-17 12:36:48 +0530

    Time: 287 ms (19.36%) | Memory: 169.2 MB (9.56%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int reversee(int num) {
-        string s = to_string(num);
+    int palat(int nums) {
+        string s = to_string(nums);
         reverse(s.begin(), s.end());
-        int j = 0;
-        while (j < s.length() && s[j] == '0') j++;
-        return stoi(s.substr(j));
+        int i = 0;
+        while (i < s.length() && s[i] == '0')
+            i++;
+        string res = s.substr(i);
+        return res.empty() ? 0 : stoi(res);
     }
-    
     int minMirrorPairDistance(vector<int>& nums) {
         unordered_map<int, vector<int>> mp;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]].push_back(i);
         }
-        
-        for (auto &p : mp) 
-            sort(p.second.begin(), p.second.end());
-        
         int ans = INT_MAX;
-        
-        for (int i = 0; i < n; i++) {
-            int rev = reversee(nums[i]);
-            if (mp.find(rev) != mp.end()) {
-                auto &vec = mp[rev];
+        for (int i = 0; i < nums.size(); i++) {
+            int num1 = palat(nums[i]);
+
+            if (mp.count(num1)) {
+                 auto &vec = mp[num1];
                 auto it = upper_bound(vec.begin(), vec.end(), i);
                 if (it != vec.end()) 
                     ans = min(ans, *it - i);
             }
         }
-        
         return ans == INT_MAX ? -1 : ans;
     }
 };
