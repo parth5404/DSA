31e1654145b6598c15bbceba6674f970ab015e93
Parth Lahoti <parthlahoti5404@gmail.com>
2026-06-14 19:19:33 +0530

    Time: 71 ms (5.83%) | Memory: 28.6 MB (5.27%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.go
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.go
@@ -10,24 +10,16 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-
-	left := lowestCommonAncestor(root.Left, p, q)
-	right := lowestCommonAncestor(root.Right, p, q)
-
 	if root == p || root == q {
 		return root
 	}
 
-	if root == p && (left == q || right == q) {
-		return root
-	}
-	if root == q && (left == p || right == p) {
-		return root
-	}
-	if (right == p && left == q) || (left == p && right == q) {
-		return root
-	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
 
+	if left != nil && right != nil {
+		return root
+	}
 	if left != nil {
 		return left
 	}
