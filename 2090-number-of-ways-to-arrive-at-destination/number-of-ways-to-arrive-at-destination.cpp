b91169b463726c48eaca627a9720276ab107eac1
Parth Lahoti <parthlahoti5404@gmail.com>
2026-07-28 23:38:33 +0530

    Time: 11 ms (44.63%) | Memory: 37.5 MB (28.83%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        int MOD = 1e9 + 7;
         unordered_map<int, vector<array<int, 2>>> mp;
         for (auto it : roads) {
             mp[it[0]].push_back({it[1], it[2]});
             mp[it[1]].push_back({it[0], it[2]});
         }
-        vector<array<long long, 2>> dist(n, {(long long)1e18, 0});
-        dist[0][0] = 0;
-        dist[0][1] = 1;
-        priority_queue<array<long long, 2>, vector<array<long long, 2>>,
-                       greater<array<long long, 2>>>
+        vector<array<long long, 2>> dist(n, {LLONG_MAX, 0});
+        dist[0] = {0, 1};
+        priority_queue<array<long long, 3>, vector<array<long long, 3>>,
+                       greater<array<long long, 3>>>
             pq;
-        pq.push({0, 0});
-        // wt,ways
+        pq.push({0, 0, 1});
         while (!pq.empty()) {
-            auto [time, node] = pq.top();
+            auto top = pq.top();
             pq.pop();
-            if (time > dist[node][0])
+            if (top[0] > dist[top[1]][0])
                 continue;
-            for (auto [v, wt] : mp[node]) {
-                if (time + wt == dist[v][0]) {
-                    dist[v][1] = (dist[v][1] + dist[node][1]) % MOD;
-                    // pq.push({dist[v][0], v});
-                }
-                if (time + wt < dist[v][0]) {
-                    dist[v][0] = time + wt;
-                    dist[v][1] = (dist[node][1]) % MOD;
-                    pq.push({dist[v][0], v});
+            for (auto it : mp[top[1]]) {
+                if (top[0] + it[1] < dist[it[0]][0]) {
+                    dist[it[0]][0] = top[0] + it[1];
+                    dist[it[0]][1] = dist[top[1]][1];
+                    pq.push({dist[it[0]][0], it[0], top[2]});
+                } else if (top[0] + it[1] == dist[it[0]][0]) {
+                    dist[it[0]][1] =
+                        (dist[it[0]][1] + dist[top[1]][1]) % (int)(1e9 + 7);
                 }
             }
         }
-
         return dist[n - 1][1];
     }
 };
