3eb8941091acdcfe447a099f2a1d5bb0a8518781
Parth Lahoti <parthlahoti5404@gmail.com>
2026-09-05 00:28:06 +0530

    Time: 16 ms (84.91%) | Memory: 139.3 MB (50.64%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!dq.empty() && nums[i] > nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
