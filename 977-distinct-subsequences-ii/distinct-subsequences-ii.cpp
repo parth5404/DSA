5fe4563c0a0f69cd47ee41a40f93ddd55965a223
Parth Lahoti <parthlahoti5404@gmail.com>
2026-09-25 00:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (89.23%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int distinctSubseqII(string s) {
-        int mod = 1e9 + 7;
         long long prev = 1;
-        vector<long long> last(26, 0);
-        last[s[0] - 'a'] = prev;
+        vector<long long> last_seen(26, 0);
+        last_seen[s[0] - 'a']++;
+        int mod = 1e9 + 7;
         for (int i = 1; i < s.length(); i++) {
-            long long oldPrev = prev;
-            int idx = s[i] - 'a';
-            prev = (prev * 2 + 1 - last[idx] + mod) % mod;
-            last[idx] = oldPrev + 1;
+            long long old_prev = prev;
+            prev = (2 * prev + 1 - last_seen[s[i] - 'a'] + mod) % mod;
+            last_seen[s[i] - 'a'] = old_prev + 1;
         }
         return prev;
     }
